25c5d73041a06d271c2635ccf25f3681d8a15e6f
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-01-23 22:50:12 +0100

    Enabling keyboard overrides

--- rules.mk
+++ rules.mk
@@ -18,12 +18,15 @@ TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 MIDI_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
 # macros: gives access to two macros
 # DYNAMIC_MACRO_ENABLE = yes
 # DM_REC1 / 2 => record/stop recording
 # DM_PLY1 / 2 => play
 # DM_RSTP => stop recording
+
+# UNICODEMAP_ENABLE = yes
 
 # From miryoku
 # MOUSEKEY_ENABLE = yes
